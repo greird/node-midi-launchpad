49da107fe2d91b61cbb04db45a3502e436d366a6
greird <h.fauconnier@gmail.com>
2016-07-05 20:35:38 +0200

    Adding test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+var midiConnector = require('./launchpad').connect();
+
+var launchpad = midiConnector.on('ready');
+
+launchpad.clear();
+
+launchpad.on("press", () => {
+	//launchpad.displayCharacter(Math.floor((Math.random() * 10)), launchpad.colors.orange.medium);
+	
+	launchpad.displayDigit(Math.floor((Math.random() * 10)), launchpad.randomColor(), "right");
+	launchpad.displayDigit(Math.floor((Math.random() * 10)), launchpad.randomColor(), "left");
+
+	//launchpad.scrollString("red team wins", 100, launchpad.colors.orange.medium, () => {
+	//	launchpad.clear();
+	//});
+})
